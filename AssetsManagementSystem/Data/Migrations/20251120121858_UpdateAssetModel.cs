6bafc7e68901623001a571fa5d02b8abf5532981
Mosaad-Ahmed <mosaad.ahmed@namaa-il.com>
2025-11-20 14:22:15 +0200

    Upload Migration

---
+++ AssetsManagementSystem/Data/Migrations/20251120121858_UpdateAssetModel.cs
@@ -0,0 +1,139 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace AssetsManagementSystem.Migrations
+{
+    /// <inheritdoc />
+    public partial class UpdateAssetModel : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<string>(
+                name: "Barcode",
+                table: "Locations",
+                type: "nvarchar(100)",
+                maxLength: 100,
+                nullable: false,
+                oldClrType: typeof(string),
+                oldType: "nvarchar(max)");
+
+            migrationBuilder.AlterColumn<string>(
+                name: "SerialNumber",
+                table: "Assets",
+                type: "nvarchar(100)",
+                maxLength: 100,
+                nullable: true,
+                oldClrType: typeof(string),
+                oldType: "nvarchar(100)",
+                oldMaxLength: 100);
+
+            migrationBuilder.UpdateData(
+                table: "AspNetRoles",
+                keyColumn: "Id",
+                keyValue: new Guid("62474474-f91b-483d-b0d8-2742c01146f0"),
+                column: "ConcurrencyStamp",
+                value: "a12a987a-52f3-4ccf-b915-e348102be61c");
+
+            migrationBuilder.UpdateData(
+                table: "AspNetRoles",
+                keyColumn: "Id",
+                keyValue: new Guid("846e3679-1537-487d-969c-3a6116fc3b2d"),
+                column: "ConcurrencyStamp",
+                value: "66f02e81-74a5-449f-a01e-539e56c1d17c");
+
+            migrationBuilder.UpdateData(
+                table: "AspNetRoles",
+                keyColumn: "Id",
+                keyValue: new Guid("d9c0c478-adf7-40db-ade3-2b7810d9659f"),
+                column: "ConcurrencyStamp",
+                value: "0e0ea0e9-c11f-4e6f-b1c6-3b32f2e85e86");
+
+            migrationBuilder.UpdateData(
+                table: "AspNetRoles",
+                keyColumn: "Id",
+                keyValue: new Guid("fc05f613-0e97-444e-b19b-018a223a7484"),
+                column: "ConcurrencyStamp",
+                value: "e27ee2bb-9c0f-4019-9b60-1e7e38c2d08e");
+
+            migrationBuilder.UpdateData(
+                table: "AspNetUsers",
+                keyColumn: "Id",
+                keyValue: new Guid("bdabcf06-a956-4ef7-8045-3214e68b9b4c"),
+                columns: new[] { "AddedOnDate", "ConcurrencyStamp", "PasswordHash", "SecurityStamp" },
+                values: new object[] { new DateTime(2025, 11, 20, 14, 18, 57, 574, DateTimeKind.Local).AddTicks(3792), "ea2f4e36-138e-4cac-bc18-62dcdcdb9932", "AQAAAAIAAYagAAAAECM5z7PGmFC8HH3uIaLckfQEoDRbtHgL0tkYPFTURGRJCF5VGBKP8XejuRG3l/sfHQ==", "d0e6e513-3316-4f65-8c3c-294de5c9c94e" });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Locations_Barcode",
+                table: "Locations",
+                column: "Barcode",
+                unique: true);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropIndex(
+                name: "IX_Locations_Barcode",
+                table: "Locations");
+
+            migrationBuilder.AlterColumn<string>(
+                name: "Barcode",
+                table: "Locations",
+                type: "nvarchar(max)",
+                nullable: false,
+                oldClrType: typeof(string),
+                oldType: "nvarchar(100)",
+                oldMaxLength: 100);
+
+            migrationBuilder.AlterColumn<string>(
+                name: "SerialNumber",
+                table: "Assets",
+                type: "nvarchar(100)",
+                maxLength: 100,
+                nullable: false,
+                defaultValue: "",
+                oldClrType: typeof(string),
+                oldType: "nvarchar(100)",
+                oldMaxLength: 100,
+                oldNullable: true);
+
+            migrationBuilder.UpdateData(
+                table: "AspNetRoles",
+                keyColumn: "Id",
+                keyValue: new Guid("62474474-f91b-483d-b0d8-2742c01146f0"),
+                column: "ConcurrencyStamp",
+                value: "4945f16d-cc88-429e-b0e0-61e534ad1b21");
+
+            migrationBuilder.UpdateData(
+                table: "AspNetRoles",
+                keyColumn: "Id",
+                keyValue: new Guid("846e3679-1537-487d-969c-3a6116fc3b2d"),
+                column: "ConcurrencyStamp",
+                value: "5bd304eb-a9fe-49f2-a2ac-e2e566bd520e");
+
+            migrationBuilder.UpdateData(
+                table: "AspNetRoles",
+                keyColumn: "Id",
+                keyValue: new Guid("d9c0c478-adf7-40db-ade3-2b7810d9659f"),
+                column: "ConcurrencyStamp",
+                value: "6a505df6-5db1-4763-b083-3ae4555007b9");
+
+            migrationBuilder.UpdateData(
+                table: "AspNetRoles",
+                keyColumn: "Id",
+                keyValue: new Guid("fc05f613-0e97-444e-b19b-018a223a7484"),
+                column: "ConcurrencyStamp",
+                value: "a3ce9f5d-a0dd-4c52-bdeb-cfb4a23e80ba");
+
+            migrationBuilder.UpdateData(
+                table: "AspNetUsers",
+                keyColumn: "Id",
+                keyValue: new Guid("bdabcf06-a956-4ef7-8045-3214e68b9b4c"),
+                columns: new[] { "AddedOnDate", "ConcurrencyStamp", "PasswordHash", "SecurityStamp" },
+                values: new object[] { new DateTime(2025, 11, 19, 10, 34, 15, 124, DateTimeKind.Local).AddTicks(3411), "66d5c98e-9c72-468a-bfdc-e1633cfbb374", "AQAAAAIAAYagAAAAEL952UXho3I/YUBy1zUVvmFxYx0PFIphVEt66T6zAh+IXinfbfeY+/IHJgjvXQylRw==", "918efb2f-51a3-4f08-bea1-2c5983c35bef" });
+        }
+    }
+}
